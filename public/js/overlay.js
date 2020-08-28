11107472989fdf79d9947aed0c79acfc17187a35
antoinelamoureux <antoinelamoureux85@icloud.com>
2020-08-29 01:39:57 +0200

    javascript added

---
+++ public/js/overlay.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    var overlay = document.querySelector(".document")
+    overlay.classList.remove('document')
+    overlay.classList.add('overlay')
+    console.log("Toutes les ressources sont chargées !")
+  })
--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,49 @@
-var links = document.getElementsByClassName('nav-option');
+var navLinks = document.getElementsByClassName('nav-link')
+
+for (let link of navLinks) {
+    link.addEventListener("click", (e) => {   
+        console.log("click")
+       e.target.style =  "border-bottom: 4px solid rgb(95, 123, 235)"
+       e.target.style += "margin-bottom: -20px"
+       e.target.style += "color: rgb(95, 123, 235)"
+    })
+}
+
+/* var links = document.getElementsByClassName('nav-option');
 function changeColor(e) {
-    e.target.style.color = e.target.style.color ? null : 'blue';
+    e.target.style.color = e.target.style.color ? null : 'red';
 }
 for (var i = 0; i < links.length; i++) {
     links[i].addEventListener('click', changeColor);
+} */
+
+/* window.addEventListener("load", () => {
+    var overlay = document.querySelector(".document")
+    overlay.classList.remove('document')
+    overlay.classList.add('overlay')
+    console.log("Toutes les ressources sont chargées !")
+  })
+ */
+
+/* var overlayLink = document.getElementsByClassName('overlay-link')
+var overlay = document.querySelector(".document")
+
+for (let link of overlayLink ) {
+    link.addEventListener("click", (e) => {   
+        if (document.querySelector(".document")) {
+        overlay.classList.remove('document')
+        overlay.classList.add('overlay')
+        console.log("loading")
+        } 
+    })
 }
+ */
+
+/* window.addEventListener("unload", () => {
+    if (document.querySelector(".overlay")) {
+        overlay.classList.remove('overlay')
+        overlay.classList.add('document')
+        console.log("unloading")
+        }
+      })
+ */
